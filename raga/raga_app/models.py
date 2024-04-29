9fa1606e1aed6f478412186fba0b4f10c3776c72
mridula-narang <narangmg@gmail.com>
2024-04-29 11:02:35 +0530

    added functionality to let admin upload audio files

--- raga/raga_app/models.py
+++ raga/raga_app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class AudioFile(models.Model):
+class Audio(models.Model):
     title = models.CharField(max_length=100)
     audio = models.FileField(upload_to='audio/')
     uploaded_date = models.DateTimeField()
     uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
