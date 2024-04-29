fcf2382dea99a3349b02dc70a21e87aad5dd8d23
mridula-narang <narangmg@gmail.com>
2024-04-29 13:38:22 +0530

    created templates and updates views.py and urls.py

--- raga/raga_app/views.py
+++ raga/raga_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+from .models import Audio
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello, World!")
+class IndexView(View):
+    def get(self, request):
+        audio = Audio.objects.order_by("uploaded_date")
+        return render(
+            request,
+            "raga_app/index.html",
+            {
+                "audio":audio,
+            }
+        )
+    
+# class AudioDetailView(View):
+#     def get(self, request, audio_id):
+#         audio = get_object_or_404(Audio, pk=audio_id)
+#         return render(
+#             request,
+#             "raga_app/audio.html",
+#             {
+#                 "audio": audio,
+#             }
+#         )
